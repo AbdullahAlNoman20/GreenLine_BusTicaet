0c02931a30d029670c9166708f3fd75bc1dd014d
Abdullah Al Noman <noman15-5387@diu.edu.bd>
2024-02-19 00:00:52 +0600

    Responsive Done

---
+++ js/body.js
@@ -0,0 +1,35 @@
+    
+    function next(){
+
+        hideElementById('homePage')
+        showElementById('success')
+
+    }
+
+    function Continue(){
+
+        hideElementById('success')
+        showElementById('homePage')
+
+
+    }
+
+    function scrollToSection(sectionId) {
+        var section = document.getElementById(sectionId);
+        if (section) {
+          section.scrollIntoView({ behavior: 'smooth' });
+        }
+      }
+
+
+      // Worked
+
+
+
+
+
+
+
+
+
+

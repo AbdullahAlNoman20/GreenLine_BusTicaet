15ece0695eb03a76e1a527d3cac57bdabaae3177
Abdullah Al Noman <noman15-5387@diu.edu.bd>
2024-02-19 20:50:43 +0600

    Dynamic Price added

--- js/seatSelection.js
+++ js/seatSelection.js
@@ -5,6 +5,7 @@ console.log("Connected");
 
 let totalButtonsCount = 39;
 let seatSelectedCount = 1;
+let totalSum = 0;
 
     function toggleSelection(button) {
         // Check the number of selected buttons
@@ -23,9 +24,14 @@ let seatSelectedCount = 1;
         if (button.classList.contains('selected')) {
             button.style.backgroundColor = '#3498db';
             button.style.color = '#fff';
+            totalSum += 550;
+
+
         } else {
             button.style.backgroundColor = '#fff';
             button.style.color = '#333';
+            totalSum -= 550;
+
         }
 
         // Update the total button count display
@@ -36,4 +42,9 @@ let seatSelectedCount = 1;
         // Update the total button count display
         const remainingSeatCount = seatSelectedCount + selectedButtons.length;
         document.getElementById('seatCount').innerText = `${remainingSeatCount}`;
+
+        // Update the total sum display
+        document.getElementById('totalSum').innerText = `${totalSum}`;
+        document.getElementById('grandTotalSum').innerText = `${totalSum}`;
+
     }
